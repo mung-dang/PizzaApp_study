4dd50d2617887eec271a8db5d2a6d706bcb444ff
윤예은 <yeayen123@gmail.com>
2022-07-29 18:23:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/pizzaapp_study/fragments/ProfileFragment.kt
@@ -0,0 +1,21 @@
+package com.example.pizzaapp_study.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.pizzaapp_study.R
+
+class ProfileFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+}
